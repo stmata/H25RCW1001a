c0ad3c4abce68d6e04d383e17b14c726b7103d6a
stmata <108939392+stmata@users.noreply.github.com>
2025-01-23 15:35:15 -0500

    Update app.js and main.js
    Remplaced app.js code and added new code to main.js

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
-const myArray = ["zero", "one", "two"];
-const myBoundMethod = function (sProperty) {
-  console.log(arguments.length > 0 ? this[sProperty] : this);
-}.bind(myArray);
+let last = 0;
+let iterations = 10;
 
-myBoundMethod(); // prints "zero,one,two" because 'this' is bound to myArray in the function
-myBoundMethod(1); // prints "one"
-setTimeout(myBoundMethod, 1.0 * 1000); // still prints "zero,one,two" after 1 second because of the binding
-setTimeout(myBoundMethod, 1.5 * 1000, "1"); // prints "one" after 1.5 seconds
+function timeout() {
+  // log the time of this call
+  log(new Date().getMilliseconds());
+  // if we are not finished, schedule the next call
+  if (iterations-- > 0) {
+    setTimeout(timeout, 0);
+  }
+}
+
+function run() {
+  // clear the log
+  const log = document.querySelector("#log");
+  while (log.lastElementChild) {
+    log.removeChild(log.lastElementChild);
+  }
+
+  // initialize iteration count and the starting timestamp
+  iterations = 10;
+  last = new Date().getMilliseconds();
+  // start timer
+  setTimeout(timeout, 0);
+}
+
+function log(now) {
+  // log the last timestamp, the new timestamp, and the difference
+  const tableBody = document.getElementById("log");
+  const logRow = tableBody.insertRow();
+  logRow.insertCell().textContent = last;
+  logRow.insertCell().textContent = now;
+  logRow.insertCell().textContent = now - last;
+  last = now;
+}
+
+document.querySelector("#run").addEventListener("click", run);
